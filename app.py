0039f78e0fd7e0db0e8b41240f71ff5eace75887
Rusty Bower <rusty@rustybower.com>
2015-06-03 22:49:55 -0500

    Adding automatically generating dictionary based on filenames. Removed debug. Added placeholder text if blurbs aren't present.

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
+from ordereddict import OrderedDict
 from flask import Flask
 from flask import render_template
+import glob
 
 app = Flask(__name__)
 
+jsondict = {}
+files = glob.glob("static/img/*.jpg")
+for file in files:
+    filename = file.split('/')[2].strip('.jpg')
+    jsondict[filename.lower()] = {'name':filename,'photo':'%s.jpg' % filename.upper(), 'text':'%s.txt' % filename}
+jsondictsorted = OrderedDict(sorted(jsondict.items(), key=lambda t: t[0]))
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="Index")
-
-jsondict = {'corsair':{'name':'Corsair','photo':'CORSAIR.jpg','text':'corsair.txt'}}
+    return render_template('index.html', title="Index", jsondict=jsondictsorted)
 
 @app.route('/<target>')
 def template(target):
-    with open('static/text/%s' % jsondict[target.lower()]['text'], 'rb') as f:
-        text = f.read().decode('utf-8')
+    try:
+        with open('static/text/%s' % jsondict[target.lower()]['text'], 'rb') as f:
+            text = f.read().decode('utf-8')
+    except IOError:
+        text = "PLACEHOLDER TEXT"
     return render_template('template.html',
                            title=jsondict[target.lower()]['name'],
                            image=jsondict[target.lower()]['photo'],
@@ -22,4 +31,4 @@ def template(target):
 
 
 
-app.run(host='0.0.0.0', debug=True)
+app.run()
